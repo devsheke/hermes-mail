8c88b1e10f4b4db0273c9531f4ef13783774b773
devsheke <abhi@sheke.tech>
2024-07-08 21:38:28 +0530

    refactor: rearrange code; feat: add helper funcs for timeouts and blocking

--- crates/mailer/src/stats.rs
+++ crates/mailer/src/stats.rs
@@ -2,31 +2,45 @@ use chrono::{DateTime, Duration, Local};
 use serde::Serialize;
 use tracing::debug;
 
-#[derive(Debug, Serialize)]
-pub(super) struct Stats {
-    pub(crate) email: String,
-    pub(crate) today: u32,
-    total: u64,
-    bounced: u64,
-    blocked: bool,
+#[derive(Debug, Serialize, Default)]
+pub(crate) struct Stats {
+    pub bounced: u64,
+    pub blocked: bool,
+    pub email: String,
     #[serde(skip_serializing)]
-    pub(crate) timeout: Option<DateTime<Local>>,
+    pub timeout: Option<DateTime<Local>>,
+    pub today: u32,
+    pub total: u64,
 }
 
 impl Stats {
     pub fn new(addr: String) -> Self {
         Self {
+            blocked: false,
+            bounced: 0,
             email: addr,
+            timeout: None,
             today: 0,
             total: 0,
-            bounced: 0,
-            blocked: false,
-            timeout: None,
         }
     }
 
-    pub fn set_timeout(&mut self, dur: Duration) {
-        self.timeout = Some(Local::now() + dur);
+    pub fn block(&mut self) {
+        self.blocked = true;
+        debug!(msg = "blocked sender", sender = self.email)
+    }
+
+    pub fn is_blocked(&self) -> bool {
+        self.blocked
+    }
+
+    pub fn inc_bounced(&mut self, amnt: u64) {
+        self.bounced += amnt;
+    }
+
+    pub fn inc_sent(&mut self, amnt: u32) {
+        self.today += amnt;
+        self.total += amnt as u64;
     }
 
     pub fn is_timed_out(&mut self) -> Option<DateTime<Local>> {
@@ -39,26 +53,16 @@ impl Stats {
         self.timeout
     }
 
-    pub fn inc_sent(&mut self, amnt: u32) {
-        self.today += amnt;
-        self.total += amnt as u64;
-    }
-
-    pub fn inc_bounced(&mut self, amnt: u64) {
-        self.bounced += amnt;
-    }
-
     pub fn reset_daily(&mut self) {
         self.today = 0;
     }
 
-    pub fn block(&mut self) {
-        self.blocked = true;
-        debug!(msg = "blocked sender", sender = self.email)
+    pub fn set_timeout(&mut self, dur: Duration) {
+        self.timeout = Some(Local::now() + dur);
     }
 
-    pub fn is_blocked(&mut self) -> bool {
-        self.blocked
+    pub fn set_timeout_if_none(&mut self, dur: Duration) {
+        self.timeout.get_or_insert(Local::now() + dur);
     }
 
     pub fn unblock(&mut self) {
